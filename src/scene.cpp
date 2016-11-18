c0c8fc9fdde648acbe78598a0f38065c9bef72cf
Mathijs Molenaar <mathijs.l.molenaar@gmail.com>
2016-11-18 21:37:47 +0100

    Scene supports planes and triangles, intersection test not yet implemented

--- src/scene.cpp
+++ src/scene.cpp
@@ -7,41 +7,92 @@ raytracer::Scene::Scene() {
 }
 
 glm::vec3 raytracer::Scene::trace_ray(const Ray& ray) const {
-	// Find the closet intersection
+	enum class Type
+	{
+		Sphere,
+		Plane,
+		Triangle
+	};
+
+	// Find the closest intersection
 	float min_intersect_time = std::numeric_limits<float>::max();
-	int i_current_hit_sphere = -1;
+	int i_current_hit = -1;
+	glm::vec3 normal_current_hit;
+	Type type_current_hit;
+	// Check sphere intersections
 	for (unsigned int i = 0; i < _spheres.size(); ++i) {
 		float intersect_time;
 		if (intersect(ray, _spheres[i], intersect_time) && intersect_time < min_intersect_time) {
 			min_intersect_time = intersect_time;
-			i_current_hit_sphere = i;
+			type_current_hit = Type::Sphere;
+			i_current_hit = i;
+		}
+	}
+	// Check plane intersections
+	for (unsigned int i = 0; i < _spheres.size(); ++i) {
+		float intersect_time;
+		if (intersect(ray, _spheres[i], intersect_time) && intersect_time < min_intersect_time) {
+			min_intersect_time = intersect_time;
+			type_current_hit = Type::Plane;
+			i_current_hit = i;
+		}
+	}
+	// Check triangle intersections
+	for (unsigned int i = 0; i < _spheres.size(); ++i) {
+		float intersect_time;
+		if (intersect(ray, _spheres[i], intersect_time) && intersect_time < min_intersect_time) {
+			min_intersect_time = intersect_time;
+			type_current_hit = Type::Triangle;
+			i_current_hit = i;
 		}
 	}
 
-	if (i_current_hit_sphere >= 0) {
-		glm::vec3 point = min_intersect_time * ray.direction + ray.origin;
-		glm::vec3 normal = glm::normalize(point - _spheres[i_current_hit_sphere].centre);
 
-		const Material& mat = _sphere_materials[i_current_hit_sphere];
+
+	if (i_current_hit >= 0) {
+		// Calculate the normal of the hit surface and retrieve the material
+		glm::vec3 point = min_intersect_time * ray.direction + ray.origin;
+		glm::vec3 normal;
+		Material material;
+		if (type_current_hit == Type::Sphere)
+		{
+			normal = glm::normalize(point - _spheres[i_current_hit].centre);
+			material = _sphere_materials[i_current_hit];
+		}
+		else if (type_current_hit == Type::Plane)
+		{
+			normal = _planes[i_current_hit].normal;
+			material = _planes_materials[i_current_hit];
+		}
+		else if (type_current_hit == Type::Triangle)
+		{
+			// TODO: check if the normal is always pointing to the correct side of the triangle
+			const glm::vec3* points = _triangles[i_current_hit].points;
+			normal = glm::cross(
+				points[1] - points[0],
+				points[2] - points[0]);
+			material = _triangles_materials[i_current_hit];
+		}
+
 		glm::vec3 colour = glm::vec3(0);
 		for (auto& light : _lights)
 		{
 			glm::vec3 lightDir;
 			if (get_light_vector(light, point, lightDir))
 			{
-				if (mat.type == Material::Type::Diffuse)
+				// TODO: move this to the material class
+				if (material.type == Material::Type::Diffuse)
 				{
-					colour += std::max(0.0f, glm::dot(normal, lightDir)) * mat.colour;
+					colour += material.colour * std::max(0.0f, glm::dot(normal, lightDir));
 				}
-				else if (mat.type == Material::Type::Mirror)
+				else if (material.type == Material::Type::Mirror)
 				{
 					Ray ray(ray.origin, glm::reflect(ray.direction, normal));
-					colour += mat.colour * trace_ray(ray);
+					colour += material.colour * trace_ray(ray);
 				}
-			}
-			else if (mat.type == Material::Type::Diffuse)
-			{
-
+				else if (material.type == Material::Type::Diffuse)
+				{
+				}
 			}
 		}
 		
--- src/scene.h
+++ src/scene.h
@@ -13,10 +13,20 @@ public:
 	Scene();
 	// traces a ray through the objects in the scene, returns a colour
 	glm::vec3 trace_ray(const Ray& ray) const;
-	template<typename TShape>
-	void add_primitive(TShape&& primitive, const Material& material) {
+
+	void add_primitive(const Sphere& primitive, const Material& material) {
 		_spheres.push_back(primitive);
 		_sphere_materials.push_back(material);
+	}
+
+	void add_primitive(const Plane& primitive, const Material& material) {
+		_planes.push_back(primitive);
+		_planes_materials.push_back(material);
+	}
+
+	void add_primitive(const Triangle& primitive, const Material& material) {
+		_triangles.push_back(primitive);
+		_triangles_materials.push_back(material);
 	}
 	
 	void add_light(Light& light)
@@ -26,6 +36,12 @@ public:
 private:
 	std::vector<Sphere> _spheres;
 	std::vector<Material> _sphere_materials;
+
+	std::vector<Plane> _planes;
+	std::vector<Material> _planes_materials;
+
+	std::vector<Triangle> _triangles;
+	std::vector<Material> _triangles_materials;
 
 	std::vector<Light> _lights;
 };
