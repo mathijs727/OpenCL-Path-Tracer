addd02d0f927848e04fa9e4dd59f49a448d14060
Mathijs Molenaar <mathijs.l.molenaar@gmail.com>
2016-11-18 18:08:26 +0100

    Fixed sphere intersection

--- src/shapes.cpp
+++ src/shapes.cpp
@@ -4,7 +4,7 @@
 
 bool raytracer::intersect(const Ray& ray, const Sphere& sphere, float& time) {
 	assert(glm::length2(ray.direction) - 1 < 1e-6); 
-	glm::vec3 distance = sphere.centre - ray.origin;
+	/*glm::vec3 distance = sphere.centre - ray.origin;
 	// calculates the projection of the distance on the ray
 	float component_parallel = glm::dot(distance, ray.direction);
 	glm::vec3 vec_parallel = component_parallel * ray.direction;
@@ -18,5 +18,17 @@ bool raytracer::intersect(const Ray& ray, const Sphere& sphere, float& time) {
 		time = component_parallel - sqrt(radius_squared - component_normal_squared);
 		return true;
 	}
-	return false;
+	return false;*/
+
+	glm::vec3 c = sphere.centre - ray.origin;
+	float t = glm::dot(c, ray.direction);
+	glm::vec3 q = c - t * ray.direction;
+	float p2 = glm::dot(q, q);
+	float r2 = sphere.radius * sphere.radius;
+	if (p2 > r2)
+		return false;
+
+	t -= sqrtf(r2 - p2);
+	time = t;
+	return true;
 }
