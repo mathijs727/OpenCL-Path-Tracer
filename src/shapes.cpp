86430da5a8c08f26427c6f12516209ca3e2786e1
Mathijs <mathijs.l.molenaar@gmail.com>
2016-11-24 16:53:48 +0100

    Triangle intersection

--- src/shapes.cpp
+++ src/shapes.cpp
@@ -75,8 +75,54 @@ bool raytracer::intersect(const Ray& ray, const Plane& plane, float& time)
 	return false;
 }
 
+// https://en.wikipedia.org/wiki/Möller–Trumbore_intersection_algorithm
 bool raytracer::intersect(const Ray& ray, const Triangle& triangle, float& time)
 {
+	const float EPSILON = 0.000001f;
+
+	glm::vec3 e1, e2;  //Edge1, Edge2
+	glm::vec3 P, Q, T;
+	float det, inv_det, u, v;
+	float t;
+
+	//Find vectors for two edges sharing V1
+	e1 = triangle.points[1] - triangle.points[0];
+	e2 = triangle.points[2] - triangle.points[0];
+
+	//Begin calculating determinant - also used to calculate u parameter
+	P = glm::cross(ray.direction, e2);
+
+	//if determinant is near zero, ray lies in plane of triangle or ray is parallel to plane of triangle
+	det = glm::dot(e1, P);
+
+	//NOT CULLING
+	if(det > -EPSILON && det < EPSILON) return false;
+	inv_det = 1.f / det;
+
+	//calculate distance from V1 to ray origin
+	T = ray.origin - triangle.points[0];
+
+	//Calculate u parameter and test bound
+	u = glm::dot(T, P) * inv_det;
+	//The intersection lies outside of the triangle
+	if(u < 0.f || u > 1.f) return false;
+
+	//Prepare to test v parameter
+	Q = glm::cross(T, e1);
+
+	//Calculate V parameter and test bound
+	v = glm::dot(ray.direction, Q) * inv_det;
+	//The intersection lies outside of the triangle
+	if(v < 0.f || u + v  > 1.f) return false;
+
+	t = glm::dot(e2, Q) * inv_det;
+
+	if(t > EPSILON) { //ray intersection
+		time = t;
+		return true;
+	}
+
+	// No hit, no win
 	return false;
 }
 
