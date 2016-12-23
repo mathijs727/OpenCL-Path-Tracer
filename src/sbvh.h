9d38900ec1f8840923f1c0d7d58fced8376af9a4
Claudio Freda <claudio.freda93@gmail.com>
2016-12-23 15:34:49 +0100

    started sbvh, commit to merge

---
+++ src/sbvh.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "types.h"
+#include <vector>
+#include "aabb.h"
+
+namespace raytracer {
+
+	class Scene;
+	struct SceneNode;
+
+	class SbvhBuilder
+	{
+	public:
+		SbvhBuilder() { };
+		~SbvhBuilder() { };
+
+		u32 build(u32 firstTriangle, u32 triangleCount);// BVH may change this
+	private:
+		void subdivide(u32 nodeId);
+		bool partition(u32 nodeId);
+		AABB createBounds(u32 triangleIndex);
+
+		u32 allocateNodePair();
+	private:
+		// Used during binned BVH construction
+		u32 _triangleOffset;
+		std::vector<glm::vec3> _centres;
+		std::vector<AABB> _aabbs;
+		std::vector<u32> _secondaryIndexBuffer;
+	};
+}
