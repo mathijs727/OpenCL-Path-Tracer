afdff8c92893b1c228ea029299139fba11d73e4e
Mathijs Molenaar <mathijs.l.molenaar@gmail.com>
2016-12-26 13:37:20 +0100

    Dont duplicate meshes

--- src/scene.cpp
+++ src/scene.cpp
@@ -9,12 +9,29 @@ raytracer::SceneNode& raytracer::Scene::add_node(std::shared_ptr<IMesh> primitiv
 	//return add_node(std::vector<Mesh>{ primitive }, transform, parent );
 	if (!parent) parent = &get_root_node();
 
+	bool found = false;
+	u32 meshId = 0;
+	for (auto& pair : _meshes)
+	{
+		if (pair.mesh.get() == primitive.get())
+		{
+			found = true;
+			break;
+		}
+		meshId++;
+	}
+
+	if (!found)
+	{
+		meshId = (u32)_meshes.size();
+		_meshes.emplace_back(primitive, 0);
+	}
+
 	auto newChild = std::make_unique<SceneNode>();
 	SceneNode& newChildRef = *newChild.get();
 	newChild->transform = transform;
 	newChild->parent = parent;	
-	newChild->mesh = (u32)_meshes.size();
-	_meshes.emplace_back(primitive, 0);
+	newChild->mesh = meshId;
 	parent->children.push_back(std::move(newChild));
 	return newChildRef;
 }
