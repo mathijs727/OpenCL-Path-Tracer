8447c40b6456da8ce705164678e2b27bc6a7dca2
mathijs727 <mathijs.l.molenaar@gmail.com>
2018-05-09 20:31:48 +0200

    Move transform implementation to cpp file

---
+++ src/transform.cpp
@@ -0,0 +1,38 @@
+#include "transform.h"
+
+namespace raytracer {
+
+Transform::Transform(glm::vec3 location, glm::quat orientation, glm::vec3 scale)
+    : location(location)
+    , orientation(orientation)
+    , scale(scale)
+{
+}
+
+glm::mat4 Transform::matrix() const
+{
+    glm::mat4 scale_matrix = glm::scale(glm::mat4(), scale);
+    glm::mat4 orientation_matrix = glm::mat4_cast(orientation);
+    glm::mat4 location_matrix = glm::translate(glm::mat4(), location);
+    return location_matrix * orientation_matrix * scale_matrix;
+}
+
+glm::mat4 Transform::rayMatrix() const
+{
+    glm::mat4 scale_matrix = glm::scale(glm::mat4(), 1.0f / scale);
+    glm::mat4 orientation_matrix = glm::mat4_cast(-orientation);
+    glm::mat4 location_matrix = glm::translate(glm::mat4(), -location);
+    return location_matrix * orientation_matrix * scale_matrix;
+}
+
+glm::vec3 Transform::transform(glm::vec3 vector) const
+{
+    return glm::vec3(matrix() * glm::vec4(vector, 1));
+}
+
+glm::vec3 Transform::transformDirection(glm::vec3 direction) const
+{
+    return glm::mat3_cast(orientation) * direction;
+}
+
+}
